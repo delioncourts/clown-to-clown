f1e828e620fb8e7db1e065aa39101b8fafb6f10f
Veronika Koksharova <91394025+delioncourts@users.noreply.github.com>
2024-05-16 01:16:14 +0300

    init: styles and html

---
+++ scripts/buttons.js
@@ -0,0 +1,8 @@
+
+
+const buttonZHMAV = document.querySelector('.zhmav');
+
+buttonZHMAV.addEventListener('click', () => {
+    buttonZHMAV.classList.toggle('button_alert');
+
+})
---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+const popup = document.querySelector('.popup');
+const popupOpenButton = document.querySelector('.cross');
+
+popupOpenButton.addEventListener('click', () => {
+            popup.classList.add('popup_opened');
+            document.addEventListener("keydown", closePopupEsc);
+            document.addEventListener("mousedown", closePopupOverlay);
+})
+
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+    document.removeEventListener("keydown", closePopupEsc);
+}
+
+function closePopupEsc(evt) {
+    if (evt.key === "Escape")
+        closePopup(document.querySelector(".popup_opened"));
+}
+
+function closePopupOverlay(evt) {
+    if (evt.target.classList.contains("popup")) {
+        closePopup(document.querySelector(".popup_opened"));
+    }
+}
